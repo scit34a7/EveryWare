625cc7fff52183ff3c55c38ba6a75b9219681a8f
WBH <wonbh0308@naver.com>
2018-03-27 10:37:19 +0900

    프로젝트 프로퍼티 java 버전 1.6 > 1.8 UserVO 유저 부서와, 유저 직위를 아이디가 아닌 이름으로 받아옴. UserVO와 User_tb의 구조가 다르니 주의! User xml selectUser > findUser로 UserController 로그인 부분 수정중

--- EveryWare/src/main/java/com/a7/everyware/user/vo/UserVO.java
+++ EveryWare/src/main/java/com/a7/everyware/user/vo/UserVO.java
@@ -2,27 +2,27 @@ package com.a7.everyware.user.vo;
 
 public class UserVO {
 	
-	String user_id;		//유저 아이디
-	String user_pw;		//유저 비밀번호
-	String user_name;	//유저 이름
-	int dept_id;		//유저 부서(부서 아이디)1
-	int position_id;	//유저 직위(직위 아이디)
-	String user_address;//유저 주소
-	String user_phone;	//유저 연락처
-	String authority;	//유저 권한
+	String user_id;			//유저 아이디
+	String user_pw;			//유저 비밀번호
+	String user_name;		//유저 이름
+	String dept_name;		//유저 부서
+	String position_name;	//유저 직위
+	String user_address;	//유저 주소
+	String user_phone;		//유저 연락처
+	String authority;		//유저 권한
 	
 	public UserVO() {
 		super();
 	}
 
-	public UserVO(String user_id, String user_pw, String user_name, int dept_id, int position_id, String user_address,
-			String user_phone, String authority) {
+	public UserVO(String user_id, String user_pw, String user_name, String dept_name, String position_name,
+			String user_address, String user_phone, String authority) {
 		super();
 		this.user_id = user_id;
 		this.user_pw = user_pw;
 		this.user_name = user_name;
-		this.dept_id = dept_id;
-		this.position_id = position_id;
+		this.dept_name = dept_name;
+		this.position_name = position_name;
 		this.user_address = user_address;
 		this.user_phone = user_phone;
 		this.authority = authority;
@@ -52,20 +52,20 @@ public class UserVO {
 		this.user_name = user_name;
 	}
 
-	public int getDept_id() {
-		return dept_id;
+	public String getDept_name() {
+		return dept_name;
 	}
 
-	public void setDept_id(int dept_id) {
-		this.dept_id = dept_id;
+	public void setDept_name(String dept_name) {
+		this.dept_name = dept_name;
 	}
 
-	public int getPosition_id() {
-		return position_id;
+	public String getPosition_name() {
+		return position_name;
 	}
 
-	public void setPosition_id(int position_id) {
-		this.position_id = position_id;
+	public void setPosition_name(String position_name) {
+		this.position_name = position_name;
 	}
 
 	public String getUser_address() {
@@ -94,10 +94,12 @@ public class UserVO {
 
 	@Override
 	public String toString() {
-		return "UserVO [user_id=" + user_id + ", user_pw=" + user_pw + ", user_name=" + user_name + ", dept_id="
-				+ dept_id + ", position_id=" + position_id + ", user_address=" + user_address + ", user_phone="
+		return "UserVO [user_id=" + user_id + ", user_pw=" + user_pw + ", user_name=" + user_name + ", dept_name="
+				+ dept_name + ", position_name=" + position_name + ", user_address=" + user_address + ", user_phone="
 				+ user_phone + ", authority=" + authority + "]";
 	}
+	
+	
 	
 	
 
